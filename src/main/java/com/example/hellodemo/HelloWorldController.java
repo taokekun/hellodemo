199df7d11588de244dc390aa04ab00f01438eff9
taokekun <66135673+taokekun@users.noreply.github.com>
2022-03-03 15:13:51 +0800

    Initial commit

---
+++ src/main/java/com/example/hellodemo/HelloWorldController.java
@@ -0,0 +1,24 @@
+package com.example.hellodemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Copyright(C),2020-2022,桃子信息网络有限公司
+ * FileName:HelloWorldController
+ * Author:  taokekun
+ * Date:    2022-03-02下午 3:41
+ * Description
+ * History:
+ * <author>   <time>   <version>   <desc>
+ * 作者姓名   修改时间   版本号     描述
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/hello")
+    public String hello(){
+        String aa = "我是新手彧";
+        return "Hello,spring boot";
+    }
+}
---
+++ src/main/java/com/example/hellodemo/HellodemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellodemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellodemoApplication.class, args);
+    }
+
+}
